c98c25b8b82473f870c827cb20c01be7bc6a115c
Jason Lu <syscallrax@gmail.com>
2017-07-17 13:19:34 -0500

    Update grub.sh
    Now rejects running on non-Darwin platforms since this is kind of a hack.
    
    It first installs mpfr and libmpc from Homebrew, then builds an entire
    GCC/Binutils cross-compile toolchain for x86_64-elf.
    
    It also builds objconv if requested since that is a prerequisite for
    building grub.
    
    Finally, using our new toolchain, we build grub and install it. The
    script then removes mpfr and libmpc since they are unneeded, and exits

--- toolchain/grub.sh
+++ toolchain/grub.sh
@@ -1,13 +1,115 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+GCCVER="gcc-7.1.0"
+BINUTILSVER="binutils-2.28"
+GCCURL="http://www.netgull.com/gcc/releases/$GCCVER/$GCCVER.tar.bz2"
+BINUTILSURL="http://ftp.gnu.org/gnu/binutils/$BINUTILSVER.tar.bz2"
+
+PREFIX="$DIR/local"
+TARGET=x86_64-elf
+
 TMPDIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
 
+BUILD_BINUTILS=false
+BUILD_GCC=false
+BUILD_OBJCONV=false
+
+function cleanup() {
+    echo ""
+    echo "Please wait, cleaning up..."
+    rm -rf "$TMPDIR"
+}
+
+function error() {
+    echo
+    echo -e "\033[31mError:\033[0m"
+}
+
+function warning() {
+    echo
+    echo -e "\033[1;33mWarning:\033[0m"
+}
+
+function download() {
+    curl -o "$2" "$1"
+}
+
+function extract() {
+    echo "Extracting $1..."
+    tar xf "$1"
+}
+
+function bail() {
+    error
+    echo "An error occurred. Please check the output and file an issue on Github"
+    exit 1
+}
+
+trap cleanup EXIT
+
+if [[ $(uname -s) != 'Darwin' ]]
+then
+    error
+    echo "This script is intended for Mac devices only! Install grub from your package manager!"
+    exit 1
+fi
+
+if [[ -f "$DIR/toolchain.sh" ]]
+then
+    error
+    echo "Please copy this script to a folder outside of the Pepper folder and run it to avoid conflicting with the LLVM toolchain"
+    exit 1
+fi
+
 pushd $TMPDIR > /dev/null
-git clone https://github.com/vertis/objconv.git
-pushd objconv > /dev/null
-g++ -o objconv -O2 src/*.cpp
-cp objconv /usr/local/bin/
-popd > /dev/null
+
+mkdir -p "$DIR/local"
+
+echo
+echo "Installing tools to $DIR/local"
+echo
+
+brew install mpfr libmpc
+
+if [[ $BUILD_BINUTILS == true ]]
+then
+    download $BINUTILSURL binutils.tar.bz2 || bail
+    extract binutils.tar.bz2 || bail
+    mkdir build-binutils
+    pushd build-binutils > /dev/null
+    ../$BINUTILSVER/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --disable-werror || bail
+    make || bail
+    make install || bail
+    popd > /dev/null
+fi
+
+if [[ $BUILD_GCC == true ]]
+then
+    download $GCCURL gcc.tar.bz2 || bail
+    extract gcc.tar.bz2 || bail
+    mkdir build-gcc
+    pushd build-gcc > /dev/null
+    ../$GCCVER/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers || bail
+    make all-gcc || bail
+    make all-target-libgcc || bail
+    make install-gcc || bail
+    make install-target-libgcc || bail
+    popd > /dev/null
+fi
+
+PATH="$DIR/local/bin:$PATH"
+
+if [[ $BUILD_OBJCONV == true ]]
+then
+    git clone https://github.com/vertis/objconv.git
+    pushd objconv > /dev/null
+    g++ -o objconv -O2 src/*.cpp
+    cp objconv /usr/local/bin/
+    popd > /dev/null
+fi
+
 git clone git://git.savannah.gnu.org/grub.git
 pushd grub > /dev/null
 ./autogen.sh
@@ -20,3 +122,5 @@ make
 make install
 popd > /dev/null
 popd > /dev/null
+
+brew uninstall mpfr libmpc
