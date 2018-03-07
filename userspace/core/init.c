48bfadcf63e70f52e63ed84e54f2b056f1c445f9
Jason Lu <syscallrax@gmail.com>
2018-03-07 14:40:28 -0600

    init: Call a system call to test

--- userspace/core/init.c
+++ userspace/core/init.c
@@ -1,5 +1,17 @@
 #include <string.h>
 
+
+#define DEFN_SYSCALL0(fn, num) \
+int syscall_##fn() \
+{ \
+ int a; \
+ asm volatile("int $0x80" : "=a" (a) : "0" (num)); \
+ return a; \
+}
+
+DEFN_SYSCALL0(test, 0);
+
 int main(int argc, char** argv)
 {
+    syscall_test();
 }
