f97b47ab832c8536ca27021e0e822ae872cc9e39
Jason Lu <syscallrax@gmail.com>
2018-03-15 09:16:05 -0400

    newlib: Exit in crt0

--- toolchain/newlib/pepper/crt0.c
+++ toolchain/newlib/pepper/crt0.c
@@ -1,5 +1,9 @@
+#include <stdlib.h>
+
 extern int main(int argc, char** argv);
 
-void _start(int argc, char** argv) {
+void _start(int argc, char** argv)
+{
     int ex = main(argc, argv);
+    exit(ex);
 }
