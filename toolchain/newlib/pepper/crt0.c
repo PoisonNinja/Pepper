7220af9169cdfe5f30b66a47f36e8b805d5ab3b2
Jason Lu <syscallrax@gmail.com>
2018-03-17 00:09:32 -0400

    newlib: Set environ

--- toolchain/newlib/pepper/crt0.c
+++ toolchain/newlib/pepper/crt0.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 
 extern int main(int argc, char** argv);
+extern char** environ;
 
-void _start(int argc, char** argv)
+void _start(int argc, char** argv, int envc, char** envp)
 {
+    environ = envp;
     int ex = main(argc, argv);
     exit(ex);
 }
