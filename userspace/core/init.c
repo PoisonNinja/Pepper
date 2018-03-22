ea0669e0c3e1865b0124916513565644155eba16
Jason Lu <syscallrax@gmail.com>
2018-03-21 19:32:57 -0500

    init: Remove test code

--- userspace/core/init.c
+++ userspace/core/init.c
@@ -6,12 +6,5 @@
 
 int main(int argc, char** argv)
 {
-    int fd = open("/sbin/init", O_RDONLY, 0);
-    if (argc == 2 && !strcmp(argv[1], "test")) {
-        syscall(42, 1, 2, 3, 4, 5);
-    }
-    if (fd == 0) {
-        syscall(20, 0xDE, 0xAD, 0xBE, 0xEE, 0xEF);
-    }
     printf("%s\n", "Hello world\n");
 }
