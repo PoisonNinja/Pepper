ef833a333f4e22354150633cc58decd24d197356
Jason Lu <syscallrax@gmail.com>
2018-03-31 18:46:07 -0500

    init: Open std* and print stuff out

--- userspace/core/init.c
+++ userspace/core/init.c
@@ -5,5 +5,11 @@
 
 int main(int argc, char** argv)
 {
-    printf("%s\n", "Hello world\n");
+    open("/dev/tty", O_RDONLY);  // stdin
+    open("/dev/tty", O_WRONLY);  // stdout
+    open("/dev/tty", O_WRONLY);  // stderr
+    printf("%d arguments passed in\n", argc);
+    for (int i = 0; i < argc; i++) {
+        printf("#%d: %s\n", i, argv[i]);
+    }
 }
