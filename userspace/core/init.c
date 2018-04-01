cfe501e9c9f7a2f5d1d4fab207d9f77e70eeb9c2
Jason Lu <syscallrax@gmail.com>
2018-03-31 20:17:40 -0500

    init: Print a hello!

--- userspace/core/init.c
+++ userspace/core/init.c
@@ -8,6 +8,7 @@ int main(int argc, char** argv)
     open("/dev/tty", O_RDONLY);  // stdin
     open("/dev/tty", O_WRONLY);  // stdout
     open("/dev/tty", O_WRONLY);  // stderr
+    printf("Hello from userspace!\n");
     printf("%d arguments passed in\n", argc);
     for (int i = 0; i < argc; i++) {
         printf("#%d: %s\n", i, argv[i]);
