d095aa14a00a1fe10ae841bce7c312882b67ed67
Jason Lu <syscallrax@gmail.com>
2018-04-09 16:32:16 -0500

    newlib.sh: Support local newlib packages

--- toolchain/newlib.sh
+++ toolchain/newlib.sh
@@ -3,5 +3,15 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Rebuilding newlib by calling toolchain.sh..."
+echo
 
-$DIR/toolchain.sh --skip-dependencies --skip-binutils --skip-gcc
+LOCAL_NEWLIB=$1
+
+if [ ! -z $LOCAL_NEWLIB ]
+then
+    echo "Using local newlib at $1"
+    echo
+    $DIR/toolchain.sh --skip-dependencies --skip-binutils --skip-gcc --local-newlib $LOCAL_NEWLIB
+else
+    $DIR/toolchain.sh --skip-dependencies --skip-binutils --skip-gcc --local-newlib
+fi
