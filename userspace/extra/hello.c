309aaf486a8101e380591fb5c59301a78328bc15
Jason Lu <syscallrax@gmail.com>
2018-04-20 23:25:25 -0500

    hello: Print out arguments

--- userspace/extra/hello.c
+++ userspace/extra/hello.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, char** argv)
 {
     printf("Hello world!\n");
+    printf("My argv and envp:\n");
+    printf("%d arguments passed in\n", argc);
+    for (int i = 0; i < argc; i++) {
+        printf("#%d: %s\n", i, argv[i]);
+    }
     return 123;
 }
