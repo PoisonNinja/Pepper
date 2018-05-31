e924faabe72fb0806edbde203954e9c8e561f7cc
Jason Lu <syscallrax@gmail.com>
2018-05-30 23:19:27 -0500

    newlib: Commonize system call numbers
    No need to maintain duplicate copies of system call numbers

---
+++ toolchain/newlib/pepper/sys/syscall_numbers.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#define SYS_read 0
+#define SYS_write 1
+#define SYS_open 2
+#define SYS_close 3
+#define SYS_stat 4
+#define SYS_fstat 5
+#define SYS_lseek 8
+#define SYS_mmap 9
+#define SYS_mprotect 10
+#define SYS_munmap 11
+#define SYS_sigaction 13
+#define SYS_sigreturn 15
+#define SYS_getpid 39
+#define SYS_fork 57
+#define SYS_execve 59
+#define SYS_exit 60
