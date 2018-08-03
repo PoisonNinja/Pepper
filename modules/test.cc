67b8532cc5704f2b73c7e2e177d58c4992aa229a
Jason Lu <syscallrax@gmail.com>
2018-08-02 21:50:37 -0500

    modules: test: Specify module metadata

--- modules/test.cc
+++ modules/test.cc
@@ -1,21 +1,19 @@
 #include <kernel.h>
 #include <kernel/init.h>
+#include <kernel/module.h>
 #include <lib/printf.h>
 #include <lib/string.h>
-
-extern void h(int a);
-
-constexpr size_t panic_max = 1024;
-
-static char panic_buffer[panic_max];
 
 extern "C" {
 int init()
 {
-    // h(5);
-    // String::memset(panic_buffer, 0, panic_max);
-    // snprintf(panic_buffer, 1024, "Hello");
-    Log::printk(Log::LogLevel::INFO, "Hello from a module!\n");
+    Log::printk(Log::LogLevel::INFO, "Hello from a module\n");
     return 0;
 }
 }
+
+MODULE_NAME("test");
+MODULE_AUTHOR("Jason Lu");
+MODULE_DESCRIPTION(
+    "A simple module to demonstrate capabilities and act as a test bed");
+MODULE_VERSION("0.0.1");
