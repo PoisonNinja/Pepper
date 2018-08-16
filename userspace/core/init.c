60d87631e4a97549f4337cfbf3834e2da4a688d4
Jason Lu <syscallrax@gmail.com>
2018-08-16 00:15:09 -0500

    userspace: core: init: Load AHCI module

--- userspace/core/init.c
+++ userspace/core/init.c
@@ -18,16 +18,30 @@ int main(int argc, char** argv)
     // for now init will be responsible for initializing it
     mknod("/dev/tty", 0644 | S_IFCHR, makedev(0, 0));
     mknod("/dev/keyboard", 0644 | S_IFCHR, makedev(1, 0));
-    open("/dev/keyboard", O_RDONLY);  // stdin
-    open("/dev/tty", O_WRONLY);       // stdout
-    open("/dev/tty", O_WRONLY);       // stderr
+    open("/dev/keyboard", O_RDONLY); // stdin
+    open("/dev/tty", O_WRONLY);      // stdout
+    open("/dev/tty", O_WRONLY);      // stderr
     printf("[init] Hello from userspace!\n");
-    int fd = open("/lib/modules/test.ko", O_RDONLY);
+    int test = open("/lib/modules/test.ko", O_RDONLY);
     struct stat st;
-    fstat(fd, &st);
-    printf("Size: %lX\n", st.st_size);
+    fstat(test, &st);
     char* buffer = malloc(st.st_size);
-    read(fd, buffer, st.st_size);
+    read(test, buffer, st.st_size);
     init_module(buffer, st.st_size, "");
     delete_module("test", 0);
+    free(buffer);
+    close(test);
+
+    int ahci = open("/lib/modules/ahci.ko", O_RDONLY);
+    fstat(ahci, &st);
+    buffer = malloc(st.st_size);
+    memset(buffer, 0, st.st_size);
+    read(ahci, buffer, st.st_size);
+    init_module(buffer, st.st_size, "");
+    free(buffer);
+    close(ahci);
+
+    // mknod("/dev/hda", 0644 | S_IFBLK, makedev(0, 0));
+    // int hda = open("/dev/hda", O_RDWR);
+    // read(hda, buffer, 10);
 }
