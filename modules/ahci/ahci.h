70a649155d31f28d323f0d271eb88e8dd5c9fd13
Jason Lu <syscallrax@gmail.com>
2018-11-09 01:23:31 -0600

    modules: ahci: Use using

--- modules/ahci/ahci.h
+++ modules/ahci/ahci.h
@@ -5,7 +5,7 @@
 #include <mm/dma.h>
 #include <types.h>
 
-typedef enum {
+using FIS_TYPE = enum {
     FIS_TYPE_REG_H2D   = 0x27, // Register FIS - host to device
     FIS_TYPE_REG_D2H   = 0x34, // Register FIS - device to host
     FIS_TYPE_DMA_ACT   = 0x39, // DMA activate FIS - device to host
@@ -14,7 +14,7 @@ typedef enum {
     FIS_TYPE_BIST      = 0x58, // BIST activate FIS - bidirectional
     FIS_TYPE_PIO_SETUP = 0x5F, // PIO setup FIS - device to host
     FIS_TYPE_DEV_BITS  = 0xA1, // Set device bits FIS - device to host
-} FIS_TYPE;
+};
 
 #define ATA_STATUS_ERR (1 << 0)  /* Error. */
 #define ATA_STATUS_DRQ (1 << 3)  /* Data Request. */
