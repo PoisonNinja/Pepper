a1e7daa4796bbbd8b46e438fd731c103228fa241
Jason Lu <syscallrax@gmail.com>
2019-02-12 22:43:53 -0600

    userspace: Clean up some code

--- userspace/core/init.c
+++ userspace/core/init.c
@@ -9,29 +9,8 @@
 #include <termios.h>
 #include <unistd.h>
 
-extern int init_module(void* module_image, unsigned long len,
-                       const char* param_values);
-extern int delete_module(const char* name, int flags);
-
 int main(int argc, char** argv)
 {
-    // Mount a new instance of tmpfs on /dev so it'll be easier to move later
-    mount("tmpfs", "/dev", "tmpfs", 0, NULL);
-
-    // Mount ptsfs
-    mkdir("/dev/pts", 0755);
-    mount("ptsfs", "/dev/pts", "ptsfs", 0, NULL);
-
-    /*
-     * Create the terminal device
-     *
-     * Eventually this will be handled by our own udev implementation, but now
-     * init will be responsible for initializing it instead.
-     */
-    mknod("/dev/fb", 0644 | S_IFCHR, makedev(1, 0));
-    mknod("/dev/keyboard", 0644 | S_IFCHR, makedev(2, 0));
-    mknod("/dev/ptmx", 0644 | S_IFCHR, makedev(5, 0));
-
     int test = open("/lib/modules/test.ko", O_RDONLY);
     struct stat st;
     fstat(test, &st);
@@ -42,24 +21,14 @@ int main(int argc, char** argv)
     free(buffer);
     close(test);
 
-    int ahci = open("/lib/modules/ahci.ko", O_RDONLY);
-    fstat(ahci, &st);
-    buffer = malloc(st.st_size);
-    memset(buffer, 0, st.st_size);
-    read(ahci, buffer, st.st_size);
-    init_module(buffer, st.st_size, "");
-    free(buffer);
-    close(ahci);
-    mknod("/dev/sda", 0644 | S_IFBLK, makedev(0, 0));
-
     int ret = fork();
     if (!ret) {
         char* const startup_argv[] = {
-            "/sbin/terminal",
-            "/usr/bin/hello",
+            "/root/sbin/terminal",
+            "/root/usr/bin/hello",
         };
         char* const envp[] = {};
-        execve("/sbin/terminal", startup_argv, envp);
+        execve("/root/sbin/terminal", startup_argv, envp);
     }
 
     return 0;
--- userspace/extra/hello.c
+++ userspace/extra/hello.c
@@ -2,10 +2,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/mount.h>
 #include <unistd.h>
-
-#include <sys/stat.h>
 
 #define WHITE "\e[77m "
 #define GRAY "\e[70m "
@@ -14,46 +11,35 @@
 
 int main(int argc, char** argv, char** envp)
 {
-    //     printf("[hello] Hello world!\n");
-    //     printf("[hello] %d arguments passed in\n", argc);
-    //     for (int i = 0; i < argc; i++) {
-    //         printf("[hello] #%d: %s\n", i, argv[i]);
-    //     }
-    //     printf("[hello] Printing out environment:\n");
-    //     for (int i = 0; envp[i]; i++) {
-    //         printf("[hello] %s\n", envp[i]);
-    //     }
-    //     printf("[hello] We also have color support :)\n");
-    //     for (char a = '1'; a < '8'; a++) {
-    //         printf("\e[3%cmTest \e[9%cmTest\n", a, a);
-    //     }
+    printf("[hello] Hello world!\n");
+    printf("[hello] %d arguments passed in\n", argc);
+    for (int i = 0; i < argc; i++) {
+        printf("[hello] #%d: %s\n", i, argv[i]);
+    }
+    printf("[hello] Printing out environment:\n");
+    for (int i = 0; envp[i]; i++) {
+        printf("[hello] %s\n", envp[i]);
+    }
+    printf("[hello] We also have color support :)\n");
+    for (char a = '1'; a < '8'; a++) {
+        printf("\e[3%cmTest \e[9%cmTest\n", a, a);
+    }
 
-    //     uint8_t* buffer = malloc(1024);
-    //     int hda         = open("/dev/hda", O_RDWR);
-    //     lseek(hda, 1030, SEEK_SET);
-    //     read(hda, buffer, 1024);
-    //     uint32_t* blocks = (uint32_t*)buffer;
-    //     printf("%08X %08X %08X %08X\n", blocks[0], blocks[1], blocks[2],
-    //     blocks[3]); free(buffer); close(hda);
-
-    mkdir("/root", 0755);
-    mount("/dev/sda", "/root", "ext2", 0, NULL);
-
-    int test = open("/root/lib/modules/test.ko", O_RDONLY);
-    struct stat st;
-    fstat(test, &st);
-    char* mod = malloc(st.st_size);
-    read(test, mod, st.st_size);
-    init_module(mod, st.st_size, "");
-    delete_module("test", 0);
-    free(mod);
-    close(test);
+    uint8_t* buffer = malloc(1024);
+    int sda         = open("/dev/sda", O_RDWR);
+    lseek(sda, 1024, SEEK_SET);
+    read(sda, buffer, 1024);
+    uint32_t* blocks = (uint32_t*)buffer;
+    printf("%08X %08X %08X %08X\n", blocks[0], blocks[1], blocks[2], blocks[3]);
+    free(buffer);
+    close(sda);
 
     int fd = open("/root/test.txt", O_RDONLY);
-    uint8_t buffer[10];
+    buffer = malloc(10);
     read(fd, buffer, 9);
     buffer[9] = '\0';
     printf("%s\n", buffer);
+    free(buffer);
 
     //     printf("\nHello MIT! This is all rendered using the userspace
     //     terminal!\n");
