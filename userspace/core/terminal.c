d3a80d994ce15e48b0055ab7971ac508eb16cd98
Jason Lu <syscallrax@gmail.com>
2019-03-16 23:42:15 -0500

    userspace: core: terminal: Use poll() to wait on both pty and keyboard
    Prepping for passing keyboard input as stdin to the shell

--- userspace/core/terminal.c
+++ userspace/core/terminal.c
@@ -1,5 +1,8 @@
 #include <fcntl.h>
+#include <poll.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
@@ -37,9 +40,9 @@ void update_cursor(int col, int row)
 
 int main(int argc, char* argv[])
 {
-    open("/dev/keyboard", O_RDONLY); // stdin
-    open("/dev/fb", O_WRONLY);       // stdout
-    open("/dev/fb", O_WRONLY);       // stderr
+    int kb = open("/dev/keyboard", O_RDONLY); // stdin
+    open("/dev/fb", O_WRONLY);                // stdout
+    open("/dev/fb", O_WRONLY);                // stderr
 
     int x = 0, y = 0;
     int bg = 0;
@@ -59,154 +62,184 @@ int main(int argc, char* argv[])
     int ret = fork();
     if (ret) {
         char buffer[128];
+        struct pollfd pfds[2];
+        pfds[0].fd     = kb;
+        pfds[0].events = POLLIN;
+        pfds[1].fd     = ptm;
+        pfds[1].events = POLLIN;
         while (1) {
-            read(ptm, buffer, 1);
-            char val = buffer[0];
-            if (val == '\n') {
-                x = 0;
-                y++;
-            } else if (val == '\r') {
-                x = 0;
-            } else if (val == '\b') {
-                if (x)
-                    x--;
-            } else if (val == '\e') {
-                read(ptm, buffer, 3);
-                val = buffer[2];
-                if (buffer[1] == '3') {
-                    switch (val) {
-                        case '0':
-                            fg = 0; // Black
-                            break;
-                        case '1':
-                            fg = 4; // Red
-                            break;
-                        case '2':
-                            fg = 2; // Green
-                            break;
-                        case '3':
-                            fg = 14; // Yellow
-                            break;
-                        case '4':
-                            fg = 1; // Blue
-                            break;
-                        case '5':
-                            fg = 5; // Magenta
-                            break;
-                        case '6':
-                            fg = 3; // Cyan
-                            break;
-                        case '7':
-                            fg = 7; // Light gray
-                            break;
-                    }
-                } else if (buffer[1] == '9') {
-                    switch (val) {
-                        case '0':
-                            fg = 8; // Dark gray
-                            break;
-                        case '1':
-                            fg = 12; // Light Red
-                            break;
-                        case '2':
-                            fg = 10; // Light Green
-                            break;
-                        case '3':
-                            fg = 14; // Yellow
-                            break;
-                        case '4':
-                            fg = 9; // Light blue
-                            break;
-                        case '5':
-                            fg = 13; // Light Magenta
-                            break;
-                        case '6':
-                            fg = 11; // Light Cyan
-                            break;
-                        case '7':
-                            fg = 15; // White
-                            break;
-                    }
-                } else if (buffer[1] == '4') {
-                    switch (val) {
-                        case '0':
-                            bg = 0; // Black
-                            break;
-                        case '1':
-                            bg = 4; // Red
-                            break;
-                        case '2':
-                            bg = 2; // Green
-                            break;
-                        case '3':
-                            bg = 14; // Yellow
-                            break;
-                        case '4':
-                            bg = 1; // Blue
-                            break;
-                        case '5':
-                            bg = 5; // Magenta
-                            break;
-                        case '6':
-                            bg = 3; // Cyan
-                            break;
-                        case '7':
-                            bg = 7; // Light gray
-                            break;
-                    }
-                } else if (buffer[1] == '7') {
-                    // read(ptm, buffer, 1);
-                    // val = buffer[0];
-                    switch (val) {
-                        case '0':
-                            bg = 8; // Dark gray
-                            break;
-                        case '1':
-                            bg = 12; // Light Red
-                            break;
-                        case '2':
-                            bg = 10; // Light Green
-                            break;
-                        case '3':
-                            bg = 14; // Yellow
-                            break;
-                        case '4':
-                            bg = 9; // Light blue
-                            break;
-                        case '5':
-                            bg = 13; // Light Magenta
-                            break;
-                        case '6':
-                            bg = 11; // Light Cyan
-                            break;
-                        case '7':
-                            bg = 15; // White
-                            break;
-                    }
-                }
+            int i = poll(&pfds, 2, -1);
+            if (i < 0) {
+                perror("Exception occurred when polling\n");
+                exit(EXIT_FAILURE);
+            }
+            if (pfds[0].revents & POLLIN) {
+                // Keyboard event
+            }
+            if (pfds[0].revents & POLLHUP) {
+                /*
+                 * Wow, how does a keyboard hang up, but oh well got to
+                 handle
+                 * it anyways
+                 */
+                perror("Keyboard hung up somehow\n");
+                exit(EXIT_FAILURE);
+            }
+            if (pfds[1].revents & POLLIN) {
                 read(ptm, buffer, 1);
-            } else {
-                size_t index           = y * VGA_WIDTH + x++;
-                uint8_t color          = ((0xF & bg) << 4) | (0xF & fg);
-                internal_buffer[index] = val | (color << 8);
-            }
-            if (x == VGA_WIDTH) {
-                x = 0;
-                y++;
-            }
-            if (y == VGA_HEIGHT) {
-                for (int ny = 1; ny < VGA_HEIGHT; ny++) {
-                    memcpy((void*)&(internal_buffer[(ny - 1) * VGA_WIDTH]),
-                           (void*)&(internal_buffer[ny * VGA_WIDTH]),
-                           2 * VGA_WIDTH);
+                char val = buffer[0];
+                if (val == '\n') {
+                    x = 0;
+                    y++;
+                } else if (val == '\r') {
+                    x = 0;
+                } else if (val == '\b') {
+                    if (x)
+                        x--;
+                } else if (val == '\e') {
+                    read(ptm, buffer, 3);
+                    val = buffer[2];
+                    if (buffer[1] == '3') {
+                        switch (val) {
+                            case '0':
+                                fg = 0; // Black
+                                break;
+                            case '1':
+                                fg = 4; // Red
+                                break;
+                            case '2':
+                                fg = 2; // Green
+                                break;
+                            case '3':
+                                fg = 14; // Yellow
+                                break;
+                            case '4':
+                                fg = 1; // Blue
+                                break;
+                            case '5':
+                                fg = 5; // Magenta
+                                break;
+                            case '6':
+                                fg = 3; // Cyan
+                                break;
+                            case '7':
+                                fg = 7; // Light gray
+                                break;
+                        }
+                    } else if (buffer[1] == '9') {
+                        switch (val) {
+                            case '0':
+                                fg = 8; // Dark gray
+                                break;
+                            case '1':
+                                fg = 12; // Light Red
+                                break;
+                            case '2':
+                                fg = 10; // Light Green
+                                break;
+                            case '3':
+                                fg = 14; // Yellow
+                                break;
+                            case '4':
+                                fg = 9; // Light blue
+                                break;
+                            case '5':
+                                fg = 13; // Light Magenta
+                                break;
+                            case '6':
+                                fg = 11; // Light Cyan
+                                break;
+                            case '7':
+                                fg = 15; // White
+                                break;
+                        }
+                    } else if (buffer[1] == '4') {
+                        switch (val) {
+                            case '0':
+                                bg = 0; // Black
+                                break;
+                            case '1':
+                                bg = 4; // Red
+                                break;
+                            case '2':
+                                bg = 2; // Green
+                                break;
+                            case '3':
+                                bg = 14; // Yellow
+                                break;
+                            case '4':
+                                bg = 1; // Blue
+                                break;
+                            case '5':
+                                bg = 5; // Magenta
+                                break;
+                            case '6':
+                                bg = 3; // Cyan
+                                break;
+                            case '7':
+                                bg = 7; // Light gray
+                                break;
+                        }
+                    } else if (buffer[1] == '7') {
+                        // read(ptm, buffer, 1);
+                        // val = buffer[0];
+                        switch (val) {
+                            case '0':
+                                bg = 8; // Dark gray
+                                break;
+                            case '1':
+                                bg = 12; // Light Red
+                                break;
+                            case '2':
+                                bg = 10; // Light Green
+                                break;
+                            case '3':
+                                bg = 14; // Yellow
+                                break;
+                            case '4':
+                                bg = 9; // Light blue
+                                break;
+                            case '5':
+                                bg = 13; // Light Magenta
+                                break;
+                            case '6':
+                                bg = 11; // Light Cyan
+                                break;
+                            case '7':
+                                bg = 15; // White
+                                break;
+                        }
+                    }
+                    read(ptm, buffer, 1);
+                } else {
+                    size_t index           = y * VGA_WIDTH + x++;
+                    uint8_t color          = ((0xF & bg) << 4) | (0xF & fg);
+                    internal_buffer[index] = val | (color << 8);
                 }
-                memset((void*)&(internal_buffer[(VGA_HEIGHT - 1) * VGA_WIDTH]),
-                       0, 2 * VGA_WIDTH);
-                y = VGA_HEIGHT - 1;
+                if (x == VGA_WIDTH) {
+                    x = 0;
+                    y++;
+                }
+                if (y == VGA_HEIGHT) {
+                    for (int ny = 1; ny < VGA_HEIGHT; ny++) {
+                        memcpy((void*)&(internal_buffer[(ny - 1) * VGA_WIDTH]),
+                               (void*)&(internal_buffer[ny * VGA_WIDTH]),
+                               2 * VGA_WIDTH);
+                    }
+                    memset(
+                        (void*)&(internal_buffer[(VGA_HEIGHT - 1) * VGA_WIDTH]),
+                        0, 2 * VGA_WIDTH);
+                    y = VGA_HEIGHT - 1;
+                }
+                lseek(1, 0, SEEK_SET);
+                write(1, (uint8_t*)internal_buffer, sizeof(internal_buffer));
+                update_cursor(x, y);
             }
-            lseek(1, 0, SEEK_SET);
-            write(1, (uint8_t*)internal_buffer, sizeof(internal_buffer));
-            update_cursor(x, y);
+            if (pfds[1].revents & POLLHUP) {
+                perror("Program exited\n");
+                // TODO: Grab exit code from child
+                exit(EXIT_FAILURE);
+            }
         }
     } else {
         dup2(0, pts);
