ff3c0fc10392cdc8fdc41fd822cedd2c4a7e7137
Jason Lu <syscallrax@gmail.com>
2019-04-15 12:13:49 -0500

    userspace: core: sh: Use getline

--- userspace/core/sh.c
+++ userspace/core/sh.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[])
 {
+    size_t buffer_size = 1024;
+    char* buffer       = malloc(buffer_size);
     while (1) {
         printf("sh> ");
-        char buffer[1024];
-        while (1) {
-            fgets(buffer, 1024, stdin);
-        }
+        getline(&buffer, &buffer_size, stdin);
+        printf("%s\n", buffer);
     }
+    free(buffer);
 }
