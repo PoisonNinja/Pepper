a677ae76dc4979c9ad080ad664d063d0b2e0135f
Jason Lu <syscallrax@gmail.com>
2019-05-27 12:33:49 -0500

    Remove support for SRC_ARCH

--- config.mk
+++ config.mk
@@ -3,14 +3,9 @@
 #################
 
 # Supported architectures
-# x86      - 32 bit x86
 # x86_64   - 64 bit x86
 ARCH ?= x86_64
 
 ####################################
 # DO NOT TOUCH ANYTHING BELOW THIS #
 ####################################
-ifeq ($(ARCH),x86)
-    ARCH := i386
-endif
-
