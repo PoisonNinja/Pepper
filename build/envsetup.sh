84bb50af9b1505c1d6524f062d729a2843ae90e3
Jason Lu <jason@jasonxlu.com>
2020-08-12 22:15:34 -0500

    build: envsetup.sh: Convert to zsh syntax

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -1,31 +1,11 @@
 #!/bin/bash
 
-# When cd is aliased or is a function (such as when RVM is installed),
-# cd doesn't behave properly. To bypass this by forcing Bash to use the
-# builtin, use command. Prefixing with a backslash is insufficient since
-# backslashes only ignore aliases, and unalias cd won't work because it is
-# a function not an alias
-
-function realpath() {
-    command cd $1; pwd
-}
-
-function croot() {
-    command cd $ROOT
-}
-
-DIR="$( command cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="${0:A:h}"
 TOOLCHAIN=$(realpath "$DIR/../toolchain")
 
-export ROOT=$(realpath "$DIR/..")
-export PATH="$TOOLCHAIN/bin:$TOOLCHAIN/local/bin:$PATH"
-export -f croot
+export PATH="$TOOLCHAIN/local/bin:$PATH"
 
-alias cproto='cproto -e -I$ROOT/mint/include'
-
-echo "ROOT = $ROOT"
 echo "PATH = $PATH"
-alias cproto
 
 echo
 echo "Environment ready"
