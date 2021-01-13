d4c68d53a76a63137259c4dd2a60686a5aed1c21
Jason Lu <jason@jasonxlu.com>
2021-01-13 15:01:30 -0500

    Revert "build: envsetup.sh: Convert to zsh syntax"
    This reverts commit 84bb50af9b1505c1d6524f062d729a2843ae90e3.

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
-DIR="${0:A:h}"
+# When cd is aliased or is a function (such as when RVM is installed),
+# cd doesn't behave properly. To bypass this by forcing Bash to use the
+# builtin, use command. Prefixing with a backslash is insufficient since
+# backslashes only ignore aliases, and unalias cd won't work because it is
+# a function not an alias
+
+function realpath() {
+    command cd $1; pwd
+}
+
+function croot() {
+    command cd $ROOT
+}
+
+DIR="$( command cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TOOLCHAIN=$(realpath "$DIR/../toolchain")
 
-export PATH="$TOOLCHAIN/local/bin:$PATH"
+export ROOT=$(realpath "$DIR/..")
+export PATH="$TOOLCHAIN/bin:$TOOLCHAIN/local/bin:$PATH"
+export -f croot
 
+alias cproto='cproto -e -I$ROOT/mint/include'
+
+echo "ROOT = $ROOT"
 echo "PATH = $PATH"
+alias cproto
 
 echo
 echo "Environment ready"
